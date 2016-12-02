1c5441e34811d4597f3fb2adce5c20ae3db22154
Andrew Mulrean <andrew@thehumangeo.com>
2016-12-02 11:18:19 -0800

    update angular to 2.2.1

--- src/app/beer-list/beer-list.component.ts
+++ src/app/beer-list/beer-list.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AngularFire, FirebaseListObservable} from 'angularfire2';
-import { Subject }           from 'rxjs/Subject';
+import {Subject}           from 'rxjs/Subject';
 
-import { BreweryService } from '../brewery.service';
-import { Beer } from '../beer';
+import {BreweryService} from '../brewery.service';
+import {Beer} from '../beer';
 
 
 @Component({
   selector: 'app-beer-list',
-  templateUrl: './beer-list.component.html',
-  styleUrls: ['./beer-list.component.css'],
+  template: `
+    <app-beer-new (onAddItem)="onAddItem($event)"></app-beer-new>
+
+    <h2>You Current Cellar:</h2>
+    
+    <md-card *ngFor="let beer of beers | async" class="md-card">
+      <md-card-subtitle *ngIf="beer.breweries && beer.breweries[0]">{{ beer.breweries[0].name }}</md-card-subtitle>
+      <md-card-title>{{ beer.name }}</md-card-title>
+      <md-card-content *ngIf="beer.style">{{ beer.style.name }}:{{ beer.abv }}%</md-card-content>
+      <md-card-actions>
+            <button md-button>EDIT</button>
+            <button md-button (click)="deleteItem(beer.$key)">DELETE</button>
+       </md-card-actions>
+    </md-card>
+  `,
+  styles: [`
+    .md-card {
+      margin: 20px;
+    }
+  `],
   providers: [BreweryService]
 })
 export class BeerListComponent implements OnInit {
@@ -17,9 +35,8 @@ export class BeerListComponent implements OnInit {
   beers: FirebaseListObservable<any>;
   private searchTerms = new Subject<string>();
 
-  constructor(
-    public af: AngularFire
-  ) {}
+  constructor(public af: AngularFire) {
+  }
 
   ngOnInit() {
     this.beers = this.af.database.list('/beers');
