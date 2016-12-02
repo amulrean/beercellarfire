43d2c73cb14e2bdaf271c76323291e5780b23a06
Andrew Mulrean <andrew@thehumangeo.com>
2016-12-02 13:57:29 -0800

    Removes more specs and consolidate files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,43 @@ import {AngularFire, FirebaseListObservable} from 'angularfire2';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: `
+    <md-sidenav-layout [class.m2app-dark]="isDarkTheme">
+
+      <md-sidenav #sidenav mode="side" class="app-sidenav">
+        Sidenav
+      </md-sidenav>
+    
+      <md-toolbar color="primary">
+        <button class="app-icon-button" (click)="sidenav.toggle()">
+          <i class="material-icons app-toolbar-menu">menu</i>
+        </button>
+    
+        Beer Cellar
+    
+        <span class="app-toolbar-filler"></span>
+        <button md-button (click)="isDarkTheme = !isDarkTheme">TOGGLE DARK THEME</button>
+        <button md-button (click)="login()" *ngIf="!(af.auth | async)">Login</button>
+        <button md-button (click)="logout()" *ngIf="af.auth | async">Logout {{ userName}}</button>
+      </md-toolbar>
+    
+      <div class="app-content">
+    
+        <div *ngIf="af.auth | async">
+          <app-beer-list></app-beer-list>
+        </div>
+      </div>
+    
+    
+    </md-sidenav-layout>
+    
+    <span class="app-action" [class.m2app-dark]="isDarkTheme">
+      <button md-fab><md-icon>add</md-icon></button>
+    </span>
+`,
+  styleUrls: [`
+
+`]
 })
 export class AppComponent {
 
